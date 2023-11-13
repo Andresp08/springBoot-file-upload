7e919d7bcc8f4d91b7f7b1b4d146c31b92d7b237
Andres Felipe Pardo Campos <andresfp292@gmail.com>
2023-11-13 11:49:29 -0500

    Implementing file upload with opencsv

---
+++ README.md
@@ -0,0 +1,4 @@
+# SpringBoot File Upload
+
+Upload files with springboot using opencsv
+
---
+++ src/main/java/com/andresoft/fileupload/student/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.andresoft.fileupload.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
